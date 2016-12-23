f37971ef45e6f35a6c3013118a1955b5361839a1
EGOmaniack <EGOmaniack@mail.ru>
2016-12-23 23:00:20 +1000

    BD
    Работа с БД

--- index.php
+++ index.php
@@ -1,5 +1,25 @@
 <?php
+$mysqli = new mysqli('localhost', 'root','','FirstBD');
+if ($mysqli->connect_errno) {
+    echo "Не удалось подключиться к MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+}
+echo $mysqli->host_info . "\n";
 
+$mysqli->set_charset("utf8");
+$sqls = "SELECT * FROM items";
+$sqlss = "INSERT INTO items(name) VALUE  ('banana');";
+
+for($i = 0; $i < 100 ; $i++){
+    $mysqli->query($sqlss);
+}
+
+$result = $mysqli->query($sqls);
+$mysqli->commit();
+//$actor = $result->fetch_assoc();
+while ($actor = $result->fetch_assoc()) {
+   $value[] = $actor;
+}
+var_dump($value);
 
 ?>
 <!DOCTYPE>
@@ -19,6 +39,8 @@
         <input type="button" value="ClickMe" id="btn">
         <br>
         <div id="cont"></div>
+        <br><br>
+
 
 </body>
 <script type="text/javascript" src="js/main.js?1"></script>
