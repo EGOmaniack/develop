5372ef3eb75c21af620d707396989d34493a234a
EGOmaniack <EGOmaniack@mail.ru>
2017-01-22 17:00:23 +1000

    xml ка

--- index.php
+++ index.php
@@ -1,28 +1,10 @@
 <?php
-echo 111;
-$mysqli = new mysqli('localhost', 'root','','FirstBD');
-if ($mysqli->connect_errno) {
-    echo "Не удалось подключиться к MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
-}
-echo $mysqli->host_info . "\n";
-
-$mysqli->set_charset("utf8");
-$sqls = "SELECT * FROM items";
-$sqlss = "INSERT INTO items(name) VALUE  ('banana');";
-
-/*for($i = 0; $i < 100 ; $i++){
-    $mysqli->query($sqlss);
-}*/
-
-$result = $mysqli->query($sqls);
-$mysqli->commit();
-//$actor = $result->fetch_assoc();
-while ($actor = $result->fetch_assoc()) {
-   $value[] = $actor;
-}
-$value[0]['id'] *= 5 ;
-var_dump($value);
-
+$file = file_get_contents("contacts.xml");
+//$file ='<p><strong>111</strong></p>';
+$xml = simplexml_load_string($file);
+$json = json_encode($xml);
+$array = json_decode($json,TRUE);
+var_dump($array);
 ?>
 <!DOCTYPE>
 
