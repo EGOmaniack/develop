5db4a230471f07b0c312765f200afaebaff5f4eb
EGOmaniack <EGOmaniack@mail.ru>
2017-02-17 09:54:45 +1000

    Construct ajax get

--- web/ajax/get_data.php
+++ web/ajax/get_data.php
@@ -4,7 +4,7 @@ $dbconn = pg_connect("host=localhost port=5432 dbname=thedata user=postgres pass
     or die('Could not connect: ' . pg_last_error());
 
 
-$result = pg_query("select * from  test.data1") or die('Ошибка запроса: ' . pg_last_error()); 
+$result = pg_query("select * from  test.data1 where id >3 AND id <= 7") or die('Ошибка запроса: ' . pg_last_error()); 
 
 $arrl=[];
 $arr=[];
