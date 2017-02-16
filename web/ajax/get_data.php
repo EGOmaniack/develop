016bf23858023571714d6522db1025ab8f59b6d5
EGOmaniack <EGOmaniack@mail.ru>
2017-02-17 08:58:29 +1000

    reformatted

---
+++ web/ajax/get_data.php
@@ -0,0 +1,28 @@
+<?php
+
+$dbconn = pg_connect("host=localhost port=5432 dbname=thedata user=postgres password=Rgrur4frg56eq16")
+    or die('Could not connect: ' . pg_last_error());
+
+
+$result = pg_query("select * from  test.data1") or die('Ошибка запроса: ' . pg_last_error()); 
+
+$arrl=[];
+$arr=[];
+while ($line = pg_fetch_assoc($result)) {
+    //echo "\t<tr>\n";
+    foreach ($line as $col_key => $col_value ) {
+        $arrl[$col_key] = $col_value;
+        //echo "\t\t<td>$col_key: $col_value;</td>\n";
+    }
+    $arr[] = $arrl;
+    //echo "\t</tr>\n";
+}
+//echo "</table>\n";
+
+pg_free_result($result);
+pg_close($dbconn);
+
+//var_dump ($arr);
+//echo ($result);
+echo json_encode($arr);
+?>
